c79c5b3a3f9d1e6c43d68611b13e08e09d9180e5
JzomitiZumatti <evgeniyshinkar@gmail.com>
2023-08-31 12:50:35 +0300

    Flexible mines.

--- Src/Field.java
+++ Src/Field.java
@@ -3,25 +3,34 @@ import java.util.Random;
 public class Field {
     Random random = new Random();
     private final int SIZE = 9;
+    private int numberOfMines;
 
     private final char[][] arr = new char[SIZE][SIZE];
 
+    public int getNumberOfMines() {
+        return numberOfMines;
+    }
+
+    public void setNumberOfMines(int numberOfMines) {
+        this.numberOfMines = numberOfMines;
+    }
+
     public char[][] createBattleField() {
         int counter = 0;
-        int dest = 10;
+        int numbOfMines = getNumberOfMines();
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
                 arr[i][j] = '.';
             }
         }
-        for (int i = 0; i < dest; i++) {
+        for (int i = 0; i < numbOfMines; i++) {
             int tempA = random.nextInt(9);
             int tempB = random.nextInt(9);
-            if (arr[tempA][tempB] != 'X' && counter < 10) {
+            if (arr[tempA][tempB] != 'X' && counter < getNumberOfMines()) {
                 arr[tempA][tempB] = 'X';
                 counter++;
             } else {
-                dest += 1;
+                numbOfMines += 1;
             }
         }
         return arr;
--- Src/Minesweeper.java
+++ Src/Minesweeper.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Minesweeper {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Field newField = new Field();
+        System.out.print("How many mines do you want on the field? > ");
+        newField.setNumberOfMines(scanner.nextInt());
         newField.createBattleField();
         newField.showBattleField();
     }

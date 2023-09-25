d8a3a6ae79eae81b7c2fca81bc83c2f1dcfba2fe
JzomitiZumatti <evgeniyshinkar@gmail.com>
2023-09-25 15:17:22 +0300

    Look around you.

--- Src/Field.java
+++ Src/Field.java
@@ -1,44 +1,82 @@
 import java.util.Random;
-
 public class Field {
-    Random random = new Random();
     private final int SIZE = 9;
-    private int numberOfMines;
+    static int numberOfMines;
+    //private String[][] arr = new String[SIZE][SIZE];
+    private char[][] arr = new char[SIZE][SIZE];
 
-    private final char[][] arr = new char[SIZE][SIZE];
-
-    public int getNumberOfMines() {
-        return numberOfMines;
-    }
-
-    public void setNumberOfMines(int numberOfMines) {
+    public Field(int numberOfMines) {
         this.numberOfMines = numberOfMines;
+        initializeField();
+        placeMines();
+        calculateNumbers();
     }
 
-    public char[][] createBattleField() {
-        int counter = 0;
-        int numbOfMines = getNumberOfMines();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+    public void initializeField() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                //arr[i][j].equals(".");
                 arr[i][j] = '.';
             }
         }
-        for (int i = 0; i < numbOfMines; i++) {
-            int tempA = random.nextInt(9);
-            int tempB = random.nextInt(9);
-            if (arr[tempA][tempB] != 'X' && counter < getNumberOfMines()) {
-                arr[tempA][tempB] = 'X';
-                counter++;
-            } else {
-                numbOfMines += 1;
-            }
-        }
-        return arr;
     }
 
-    public void showBattleField() {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+    public void placeMines() {
+        Random random = new Random();
+        int minesPlaced = 0;
+        while (minesPlaced < numberOfMines) {
+            int row = random.nextInt(SIZE);
+            int col = random.nextInt(SIZE);
+//            if (!arr[row][col].equals("X")) {
+//                arr[row][col] = "X";
+//                minesPlaced++;
+//            }
+            if (arr[row][col] != 'X') {
+                arr[row][col] = 'X';
+                minesPlaced++;
+            }
+        }
+    }
+
+    public void calculateNumbers() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+//                if (!arr[i][j].equals("X")) {
+//                    int count = countAdjacentMines(i, j);
+//                    if (count > 0) {
+//                        arr[i][j] = String.valueOf(count);
+//                    }
+//                }
+                if (arr[i][j] != 'X') {
+                    int count = countAdjacentMines(i, j);
+                    if (count > 0) {
+                        arr[i][j] = String.valueOf(count).charAt(0);
+                    }
+                }
+            }
+        }
+    }
+
+    public int countAdjacentMines(int row, int col) {
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                int newRow = row + i;
+                int newCol = col + j;
+//                if (newRow >= 0 && newRow < SIZE && newCol >= 0 && newCol < SIZE && arr[newRow][newCol].equals("X")) {
+//                    count++;
+//                }
+                if (newRow >= 0 && newRow < SIZE && newCol >= 0 && newCol < SIZE && arr[newRow][newCol] == 'X') {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public void displayField() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 System.out.print(arr[i][j]);
             }
             System.out.println();
--- Src/Minesweeper.java
+++ Src/Minesweeper.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Minesweeper {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Field newField = new Field();
         System.out.print("How many mines do you want on the field? > ");
-        newField.setNumberOfMines(scanner.nextInt());
-        newField.createBattleField();
-        newField.showBattleField();
+        Field.numberOfMines = scanner.nextInt();
+        Field newField = new Field(Field.numberOfMines);
+        newField.displayField();
     }
 }
